d84281500c88f55c8fa5fa569d14f20ba6a14752
harshpatel2405 <harshpatel4504@gmail.com>
2026-05-04 19:29:41 +0530

    Basics(cout , cin)

---
+++ Basic/cout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 12;
+    float marks = 25;
+
+    string name = "Harsh";
+    bool isActive = true;
+    cout << "Hello World\n";
+    cout << "My age is " << age << endl;
+    cout << "My marks are " << marks << endl;
+    return 0;
+}
